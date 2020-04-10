8f61372e58e781245ecde8ec3a107802a219e650
Jordan <jordan.hewitta7@gmail.com>
2020-04-10 13:23:27 -0600

    Updated profile form

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email
 
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired()])
-    password = PasswordField('Password', validators=[InputRequired()])
+class ProfileForm(FlaskForm):
+    first_name = StringField('First Name', validators = [DataRequired()])
+    last_name = StringField('Last Name', validators = [DataRequired()])
+    gender = SelectField('Gender', choices = [('Male','male'),('Female','female')])
+    email = StringField('Email',validators = [DataRequired()],Email())
+    location = StringField('Location', validators = [DataRequired()])
+    biography = TextAreaField('Biography', validators = [DataRequired()])
+    profile_pic = FileField('Profile Picture', validators = [FileRequired(), FileAllowed(['jpg','jpeg','png'], 'Only images please.')])
+    submit = SubmitField("Add Profile")
